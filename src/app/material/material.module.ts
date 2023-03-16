969398cbba874061a748beb89f61faf32bb30678
Antmendes <97694520+Antmendes@users.noreply.github.com>
2023-03-16 20:09:45 -0300

    criando o footer e iniciando o header

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
-
-
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @NgModule({
-  exports: []
+  exports: [
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressBarModule
+  ]
 })
 export class MaterialModule { }
